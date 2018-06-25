79d6be5f3f51dce37bf275a860d446dc20207cde
Dearast <damien.clement.czech@gmail.com>
2018-06-25 14:41:05 +0200

    Matrix

--- Training-04/Program.cs
+++ Training-04/Program.cs
@@ -11,11 +11,11 @@ namespace Training_04
 		static void Main(string[] args)
 		{
 			Console.Title = "Training by KeenMate";
-			Write("Training by KeenMate | Done by Damien Clément", ConsoleColor.Green, true, true);
-			Matrix();
+			WriteLine("Training by KeenMate | Done by Damien Clément", ConsoleColor.Green, true, true);
+			FirstMatrix();
 		}
 
-		public static void Write(string text, ConsoleColor color = ConsoleColor.White, bool center = false, bool backToDefault = false, ConsoleColor backColor = ConsoleColor.Black)
+		public static void WriteLine(string text, ConsoleColor color = ConsoleColor.White, bool center = false, bool backToDefault = false, ConsoleColor backColor = ConsoleColor.Black)
 		{
 			Console.ForegroundColor = color;
 			Console.BackgroundColor = backColor;
@@ -36,11 +36,78 @@ namespace Training_04
 			}
 		}
 
-		public static void Matrix()
+		public static void Write(string text, int divided,bool center = false, bool backToDefault = false, ConsoleColor backColor = ConsoleColor.Black)
 		{
-			Write("Write count rows");
+			float number = 20 / divided;
+			int[] max = new int[(int)number];
+			int[] min = new int[(int)number];
+			int dividedNumber = Math.DivRem(20, divided, out dividedNumber);
+			if(dividedNumber == 0)
+			{
+				for (int i = 0; i < number; i++)
+				{
+					max[i] = (int)(number * i);
+					min[i] = (int)((number * i) - 4);
+				}
+			}
+			else
+			{
+
+			}
+			for (int i = 0; i < number; i++)
+			{
+				if (int.Parse(text) >= min[i] && int.Parse(text) <= max[i])
+				{
+					if(i == 0)
+					{
+						Console.ForegroundColor = ConsoleColor.Red;
+					}
+					else if(i == 1)
+					{
+						Console.ForegroundColor = ConsoleColor.Blue;
+					}
+					else if(i == 2)
+					{
+						Console.ForegroundColor = ConsoleColor.Yellow;
+					}
+					else if(i == 3)
+					{
+						Console.ForegroundColor = ConsoleColor.Green;
+					}
+					else if(i == 4)
+					{
+						Console.ForegroundColor = ConsoleColor.Magenta;
+					}
+					else if(i == 5)
+					{
+						Console.ForegroundColor = ConsoleColor.Cyan;
+					}
+				}
+			}
+
+			Console.BackgroundColor = backColor;
+			if (center)
+			{
+				int screenWidth = Console.WindowWidth;
+				int stringWidth = text.Length;
+				int spaces = (screenWidth / 2) + (stringWidth / 2);
+				Console.Write(text.PadLeft(spaces));
+			}
+			else
+			{
+				Console.Write(text);
+			}
+			if (backToDefault)
+			{
+				Console.ForegroundColor = ConsoleColor.White;
+			}
+		}
+
+		public static void FirstMatrix()
+		{
+			WriteLine("Write count rows");
 			int rows = int.Parse(Console.ReadLine());
-			Write("Write count collumn");
+			WriteLine("Write count collumn");
 			Random r = new Random();
 			int Collumn = int.Parse(Console.ReadLine());
 			int[,] array = new int[Collumn, rows];
@@ -53,7 +120,7 @@ namespace Training_04
 				{
 					array[x,y] = r.Next(0, 20);
 #if DEBUG
-					Write("Number in array - [" + x + ";" + y + "] is = " + array[x, y],ConsoleColor.Blue,true);
+					WriteLine("Number in array - [" + x + ";" + y + "] is = " + array[x, y],ConsoleColor.Blue,true);
 #endif
 					text += array[x, y].ToString();
 					if(array[x, y] < 10)
@@ -65,10 +132,17 @@ namespace Training_04
 						text += " ";
 					}
 				}
-				Write(text, ConsoleColor.Green, true, true);
+				WriteLine(text, ConsoleColor.Green, true, true);
 				text = string.Empty;
 			}
-			Console.ReadKey();
+			SecondMatrix();
+		}
+
+		public static void SecondMatrix()
+		{
+			WriteLine("Write how much the number will divided");
+			int check = int.Parse(Console.ReadLine());
+
 		}
 	}
 }
