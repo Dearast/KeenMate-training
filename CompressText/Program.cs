8a09e769d9adc9bd2adbfc9b7f9b73d7b779691f
Dearast <damien.clement.czech@gmail.com>
2018-07-04 09:59:27 +0200

    prepare compressing

---
+++ CompressText/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CompressText
+{
+	class Program
+	{
+		public static string defaultFilePath = Path.Combine(Directory.GetCurrentDirectory(), "\\text.txt");
+		public static string compressedFilePath = Path.Combine(Directory.GetCurrentDirectory(), "\\compressedText.txt");
+		public static string[] DuplicatedWords;
+
+		static void Main(string[] args)
+		{
+			Start();
+		}
+
+		public static void Start()
+		{
+
+		}
+
+		public static void FindDuplicate()
+		{
+
+		}
+
+		public static void CreateKey()
+		{
+
+		}
+
+		public static void CompressingText()
+		{
+
+		}
+	}
+}
